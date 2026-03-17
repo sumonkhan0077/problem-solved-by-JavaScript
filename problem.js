c5f96bb1cd68d11cad7fb26f82eaa902253b921a
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-03-17 22:51:49 +0600

    added card products problem add

--- problem.js
+++ problem.js
@@ -1,9 +1,36 @@
-// const api1 = "dadfsfsd"
-// const api2= "asdfgj"
+const cart = [
+  { name: "Shirt", price: 500, quantity: 2 },
+  { name: "Pant", price: 800, quantity: 1 },
+  { name: "Shoes", price: 1200, quantity: 1 }
+];
+// let price = 0;
+
+// for (let product of cart ) {
+//    price = price + product.price * product.quantity
+   
+// }
+const originalTotal = cart.reduce((sum, item) => {
+  return sum + (item.price * item.quantity);
+}, 0);
 
 
-// const fatchData = async () => {
-//   const res = await fetch("/api1");
-//   return res.json();
-// };
+const price = cart.reduce((sum , item)=> {
+    let itemTotal = item.price * item.quantity
 
+    let extraDiscount = 0;
+
+    if (item.quantity >= 2 ) {
+         extraDiscount= itemTotal * .05
+    }
+       return sum = (itemTotal - extraDiscount)  + sum
+},0)
+
+// main discount
+let totalPrice = price;
+if (price > 1000) {
+   discount= price * .10
+   totalPrice = price - discount
+}
+
+console.log("Total Price:",originalTotal)
+console.log("Final price:", totalPrice)
